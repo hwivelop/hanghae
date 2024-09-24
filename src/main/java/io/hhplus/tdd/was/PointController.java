3bd79125eb97f339b5bc553eada0d510e396c396
whee <hwiveloper17@naver.com>
2024-09-25 00:21:37 +0900

    [feature] Package 구조 변경

--- src/main/java/io/hhplus/tdd/was/PointController.java
+++ src/main/java/io/hhplus/tdd/was/PointController.java
@@ -1,5 +1,6 @@
-package io.hhplus.tdd.point;
+package io.hhplus.tdd.was;
 
+import io.hhplus.tdd.point.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
